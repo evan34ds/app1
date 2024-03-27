6ca55302d8808fdbd4c1a86671bc879d90414067
evan34ds <102297116+evan34ds@users.noreply.github.com>
2024-03-27 12:15:40 +0800

    mengembalikan tanggal

--- app/Views/admin/pembayaran/v_laporan_pembayaran.php
+++ app/Views/admin/pembayaran/v_laporan_pembayaran.php
@@ -19,7 +19,7 @@
             $no = 1;
             $total_pelunasan = 0;
             foreach ($results as $key => $value) {
-                $today = date_indo('Y-m-d 03:01:00');
+                $today = date_indo('Y-m-d');
                 $total_pelunasan = $total_pelunasan + $value['pelunasan']; //menjumlahkan sks
                 
             ?>
